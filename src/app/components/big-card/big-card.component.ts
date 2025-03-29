be1c2bae8aa7f1c94df7f0d8a9c5f5edeac7a90c
Paulo Santos <paulorfsantos17@gmail.com>
2025-03-29 12:49:31 -0300

    feat: add components porperty dinamics

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string =  ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription = ""
+
+  constructor() {}
 
 }
